60e1f5bfcafd76c47f52e9e511eaee0ad8da58fa
zhilutianji <starmars_lu@163.com>
2023-07-29 03:45:27 +0800

    feat:fix css & import tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css:['normalize.css/normalize.css'],
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
   vite: {
     css: {
       preprocessorOptions: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,32 @@
 <template>
-  <main class="index">
-    <section class="content">
-      <h1>💖 Hello. </h1>
-      <h2>I am zhilutianji , a Front-end developer.</h2>
-      <p>You can find me in Twitter: <a href="https://twitter.com/zhilutianji">@zhilutianji</a>
-        or Github: <a href="https://github.com/starNGC2237">@starNGC2237</a></p>
-        <p>and sometimes I also solve problems on  <a href="https://leetcode.cn/u/zhilutianji/">leetcode</a></p>
+  <main class="absolute w-full h-full flex bg-[#fff] pb-[7.5em]">
+    <section class="content h-full bg-[#fff] flex flex-col justify-center box-border text-4xl">
+      <h1 class="text-5xl my-7 font-bold">💖 Hello. </h1>
+      <h2 class="text-3xl my-6 font-bold">I am zhilutianji , a Front-end developer.</h2>
+      <p class="text-xl my-5">You can find me in Twitter: <a target="_blank" class="text-[#87ceeb] no-underline hover:underline" href="https://twitter.com/zhilutianji">@zhilutianji</a>
+        or Github: <a target="_blank" class="text-[#87ceeb] no-underline hover:underline" href="https://github.com/starNGC2237">@starNGC2237</a></p>
+      <p class="text-xl my-5">and sometimes I also solve problems on  <a target="_blank" class="text-[#87ceeb] no-underline hover:underline" href="https://leetcode.cn/u/zhilutianji/">leetcode</a></p>
     </section>
-    <section class="head">
-      <img src="@/assets/images/head.png" alt="head pic"/>
+    <section class="head h-full bg-[#fff] flex items-center justify-end box-border ">
+      <img class="rounded-full" src="@/assets/images/head.png" alt="head pic"/>
     </section>
   </main>
 </template>
 <style scoped lang="scss">
-.index {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  display: flex;
-}
 .content{
   width: 55%;
-  height: 100%;
-  background-color: #fff;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  box-sizing: border-box;
   padding: 0 0 0 10vw;
-  font-size: 20px;
-  a{
-    color: skyblue;
-  }
 }
 .head{
   width: 45%;
-  height: 100%;
-  background-color: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: right;
-  box-sizing: border-box;
   padding-right: 10vw;
   img{
     width: 46%;
     @media screen and (max-width: 1024px) {
       width: 80%;
     }
-    border-radius: 50%;
   }
 }
-
 </style>
 <script setup lang="ts">
 </script>
