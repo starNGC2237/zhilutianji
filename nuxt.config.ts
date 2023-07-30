10ae71985b6ebb70b9443844f3d0803cd1c50b46
zhilutianji <starmars_lu@163.com>
2023-07-30 23:54:37 +0800

    feat:add more meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,15 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-16',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'starlibrary.site',
+      title: 'zhilutianji\'s site',
       meta: [
         // <meta name="description" content="My amazing site">
-        { name: 'description', content: 'zhilutianji \'s amazing site.' }
+        { name: 'description', content: 'zhilutianji \'s amazing site.' },
+        { name: 'author', content: 'zhilutianji' },
+        { name: 'keywords', content: 'zhilutianji, zhilutianji\'s site, zhilutianji\'s about, zhilutianji\'s homepage,zhilutianji\'s github ' },
+        { name: 'robots', content: 'index,follow' },
+        { name: 'googlebot', content: 'index,follow' },
+        { name: 'google', content: 'notranslate' },
       ],
     }
   },
