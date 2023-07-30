c22d0a09236e0240863b84ac0fd00b4933f15176
zhilutianji <starmars_lu@163.com>
2023-07-31 02:33:14 +0800

    fix:The background color and foreground color do not have a high enough contrast.

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <section class="w-[55%] h-full bg-[#fff] flex flex-col justify-center box-border text-4xl">
       <h1 class="text-5xl my-7 font-bold">💖 Hello. </h1>
       <h2 class="text-3xl my-6 font-bold">I am zhilutianji , a Front-end developer.</h2>
-      <p class="text-xl my-5">You can find me in Twitter: <a target="_blank" class="text-[#87ceeb] no-underline hover:underline" href="https://twitter.com/zhilutianji">@zhilutianji</a>
-        or Github: <a target="_blank" class="text-[#87ceeb] no-underline hover:underline" href="https://github.com/starNGC2237">@starNGC2237</a></p>
-      <p class="text-xl my-5">and sometimes I also solve problems on  <a target="_blank" class="text-[#87ceeb] no-underline hover:underline" href="https://leetcode.cn/u/zhilutianji/">LeetCode</a></p>
+      <p class="text-xl my-5">You can find me in Twitter: <a target="_blank" class="text-[#0000CD] no-underline hover:underline" href="https://twitter.com/zhilutianji">@zhilutianji</a>
+        or Github: <a target="_blank" class="text-[#0000CD] no-underline hover:underline" href="https://github.com/starNGC2237">@starNGC2237</a></p>
+      <p class="text-xl my-5">and sometimes I also solve problems on  <a target="_blank" class="text-[#0000CD] no-underline hover:underline" href="https://leetcode.cn/u/zhilutianji/">LeetCode</a></p>
     </section>
     <section class="w-[45%] h-full bg-[#fff] flex items-center justify-end box-border ">
       <nuxt-img preload class="rounded-full xl:w-[40%] lg:w-[55%] md:w-[65%] sm:w-[80%] w-[80%] h-auto" width="80%" height="auto" src="/head.png" alt="head pic"/>
