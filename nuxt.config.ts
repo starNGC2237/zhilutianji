6e85ad02148d4abd37517387edc3dfe50abec2c5
zhilutianji <starmars_lu@163.com>
2023-07-31 00:51:56 +0800

    fix: update meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-16',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'zhilutianji\'s site',
+      title: 'zhilutianji \'s site',
       meta: [
         // <meta name="description" content="My amazing site">
         { name: 'description', content: 'zhilutianji \'s amazing site.' },
