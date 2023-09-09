1ccc8ae45844556baa32a72fdf06674371a13845
zhilutianji <starmars_lu@163.com>
2023-09-09 23:09:37 +0800

    fix: addd magin

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -8,11 +8,11 @@ const { data: blogList,error } = await useAsyncData(`content`, () => {
   <main class="absolute w-full min-h-full bg-white dark:bg-neutral-900">
     <section class="flex justify-center w-full min-h-screen">
       <div class="container pb-[8em] pt-[3em]">
-        <h1 class="px-4 text-4xl mb-7 w-[100%] sm:w-[90%] dark:text-[#e8e6e3]">Blog:</h1>
+        <h1 class="px-4 text-4xl mb-7 w-[100%] sm:w-[90%] dark:text-[#e8e6e3] m-auto">Blog:</h1>
         <div v-if="error">
           {{error}}
         </div>
-        <div class="flex w-full flex-wrap justify-around px-4" v-if="blogList">
+        <div class="flex w-full flex-wrap justify-around px-4 sm:w-[90%] m-auto" v-if="blogList">
           <div class="p-6 bg-white w-[100%] sm:w-[100%] flex flex-col justify-left item rounded-lg border bg-opacity-70 backdrop-blur-sm hover:shadow-md" v-for="item in blogList" :key="item['id']">
             <h2 class="text-2xl mb-3"><a :href="item['_path']" class="font-bold">{{item['title']}}</a></h2>
             <p>{{item['description'] === '' ? '( no description )':item['description']}}</p>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <main class="absolute w-full h-full flex bg-[#fff] pb-[8em] px-[10%] dark:bg-neutral-900">
-    <section class="w-[55%] h-full flex flex-col justify-center box-border text-4xl">
-      <h1 class="text-5xl my-7 font-bold dark:text-[#e8e6e3]">💖 Hello. </h1>
-      <h2 class="text-3xl my-6 font-bold dark:text-[#e8e6e3]">I am zhilutianji , a Front-end developer.</h2>
-      <p class="text-xl my-5 dark:text-[#e8e6e3]">You can find me in Github: <a target="_blank" class="text-[#0000CD] dark:text-blue-500 no-underline hover:underline" href="https://github.com/starNGC2237">@starNGC2237</a></p>
-      <p class="text-xl my-5 dark:text-[#e8e6e3]">and sometimes I also solve problems on  <a target="_blank" class="text-[#0000CD] dark:text-blue-500 no-underline hover:underline" href="https://leetcode.cn/u/zhilutianji/">LeetCode</a></p>
+  <main class="absolute w-full h-full flex bg-[#fff]  dark:bg-neutral-900 justify-center">
+    <section class="container flex pb-[8em] px-[2em]">
+      <section class="w-[55%] h-full flex flex-col justify-center box-border text-4xl">
+        <h1 class="text-5xl my-7 font-bold dark:text-[#e8e6e3]">💖 Hello. </h1>
+        <h2 class="text-3xl my-6 font-bold dark:text-[#e8e6e3]">I am zhilutianji , a Front-end developer.</h2>
+        <p class="text-xl my-5 dark:text-[#e8e6e3]">You can find me in Github: <a target="_blank" class="text-[#0000CD] dark:text-blue-500 no-underline hover:underline" href="https://github.com/starNGC2237">@starNGC2237</a></p>
+        <p class="text-xl my-5 dark:text-[#e8e6e3]">and sometimes I also solve problems on  <a target="_blank" class="text-[#0000CD] dark:text-blue-500 no-underline hover:underline" href="https://leetcode.cn/u/zhilutianji/">LeetCode</a></p>
+      </section>
+      <section class="w-[45%] h-full flex items-center justify-end box-border ">
+        <nuxt-img preload class="rounded-full xl:w-[40%] lg:w-[55%] md:w-[65%] sm:w-[80%] w-[80%] h-auto" width="80%" height="auto" src="/head.png" alt="head pic"/>
+      </section>
     </section>
-    <section class="w-[45%] h-full flex items-center justify-end box-border ">
-      <nuxt-img preload class="rounded-full xl:w-[40%] lg:w-[55%] md:w-[65%] sm:w-[80%] w-[80%] h-auto" width="80%" height="auto" src="/head.png" alt="head pic"/>
-    </section>
+
   </main>
 </template>
+<script setup lang="ts">
+</script>
