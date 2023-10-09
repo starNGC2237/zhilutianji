72063e4ae9a4ac5a8c468a65ba690558a15815a3
zhilutianji <starmars_lu@163.com>
2023-10-09 22:27:50 +0800

    update:images flow

---
+++ pages/dev.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="absolute w-full min-h-full bg-white  dark:bg-neutral-900">
+    <section class="flex justify-center w-full min-h-screen">
+      <div class="container pb-[8em] pt-[3em]">
+        <h1 class="px-4 text-4xl mb-7 w-[100%] sm:w-[90%] m-auto dark:text-[#e8e6e3]">照片流:</h1>
+        <div class="flex w-full flex-wrap justify-around px-4">
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped lang="postcss">
+
+</style>
